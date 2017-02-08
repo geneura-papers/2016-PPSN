36bc3d8a005dc4a136b7577a151fe343b0361802
unintendedbear <unintendedbear@gmail.com>
2017-02-08 16:21:26 +0100

    Adjusting the scale

--- GPrules/dataprocessing.R
+++ GPrules/dataprocessing.R
@@ -4,6 +4,9 @@ require(ggplot2)
 require(dplyr)
 require(reshape2)
 require(xtable)
+library(gridExtra)
+library(grid)
+library(lattice)
 
 read_data <- function(directory) {
   
@@ -23,6 +26,11 @@ alpha0Data <- read_data("./CSVs/a = 0")
 alpha05Data <- read_data("./CSVs/a = 0.5")
 alpha1Data <- read_data("./CSVs/a = 1")
 
-theFitness <- ggplot(alpha0Data, aes(x = alpha0Data$IT, y = alpha0Data$BEST_F)) +
-  geom_point(aes(colour = factor(FOLD))) + stat_summary()
-print(theFitness)
+Fa0 <- ggplot(alpha0Data, aes(x = alpha0Data$IT, y =BEST_F/47966)) +
+  geom_point(aes(colour = factor(FOLD))) + stat_summary() + scale_y_continuous(limits = c(0,0.5)) + theme(legend.position = "none")
+Fa05 <- ggplot(alpha05Data, aes(x = alpha05Data$IT, y = alpha05Data$BEST_F/47966)) +
+  geom_point(aes(colour = factor(FOLD))) + stat_summary() + scale_y_continuous(limits = c(0,0.5))  + theme(legend.position = "none")
+Fa1 <- ggplot(alpha1Data, aes(x = alpha1Data$IT, y = alpha1Data$BEST_F/47966)) +
+  geom_point(aes(colour = factor(FOLD))) + stat_summary() + scale_y_continuous(limits = c(0,0.5))  + theme(legend.position = "none")
+
+grid.arrange(Fa0, Fa05, Fa1, ncol = 3)
