c319c7bcc4ef1a3e77cf108892c533419445b780
unintendedbear <unintendedbear@gmail.com>
2017-02-14 12:32:45 +0100

    Experiments are done, so this closes #21.

--- GPrules/dataprocessing.R
+++ GPrules/dataprocessing.R
@@ -22,15 +22,27 @@ read_data <- function(directory) {
   return(allData)
 }
 
+get_legend<-function(myggplot){
+  tmp <- ggplot_gtable(ggplot_build(myggplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
+
 alpha0Data <- read_data("./CSVs/a = 0")
 alpha05Data <- read_data("./CSVs/a = 0.5")
 alpha1Data <- read_data("./CSVs/a = 1")
 
 Fa0 <- ggplot(alpha0Data, aes(x = alpha0Data$IT, y =BEST_F/47966)) +
-  geom_point(aes(colour = factor(FOLD))) + stat_summary() + scale_y_continuous(limits = c(0,0.5)) + theme(legend.position = "none")
+  geom_point(aes(colour = factor(FOLD))) + stat_summary() + scale_y_continuous(limits = c(0,0.5)) + theme(legend.position = "bottom")
+#theLegend <- get_legend(Fa0)
+Fa0 <- Fa0 + theme(legend.position = "none")
 Fa05 <- ggplot(alpha05Data, aes(x = alpha05Data$IT, y = alpha05Data$BEST_F/47966)) +
   geom_point(aes(colour = factor(FOLD))) + stat_summary() + scale_y_continuous(limits = c(0,0.5))  + theme(legend.position = "none")
 Fa1 <- ggplot(alpha1Data, aes(x = alpha1Data$IT, y = alpha1Data$BEST_F/47966)) +
   geom_point(aes(colour = factor(FOLD))) + stat_summary() + scale_y_continuous(limits = c(0,0.5))  + theme(legend.position = "none")
 
+#grid.arrange(Fa0, Fa05, Fa1, theLegend, ncol = 3, nrow = 2, layout_matrix = cbind(c(1, 4), c(2, 4), c(3, 4)))
 grid.arrange(Fa0, Fa05, Fa1, ncol = 3)
+
+             
