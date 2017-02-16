e540bc2cc3c3a76a9ef1b36347660b2052d19443
unintendedbear <unintendedbear@gmail.com>
2017-02-16 21:38:34 +0100

    Adapting the first script and reading all data, now ready for graphs

--- GPrules/datapreparation.R
+++ GPrules/datapreparation.R
@@ -13,16 +13,16 @@ add_fold_column <- function(directory) {
   }
 }
 
-treeIndAlpha05Data <- add_fold_column("./lanzar1/CSVs")
-treeIndAlpha0Data <- add_fold_column("./lanzar2/CSVs")
-treeIndAlpha1Data <- add_fold_column("./lanzar3/CSVs")
-treeIndCovData <- add_fold_column("./lanzar4/CSVs")
+add_fold_column("./lanzar1/CSVs")
+add_fold_column("./lanzar2/CSVs")
+add_fold_column("./lanzar3/CSVs")
+add_fold_column("./lanzar4/CSVs")
 
-listIndCovAllowData <- add_fold_column("./lanzar5/150gen GRANTED/CSVs")
-listIndCovDenyData <- add_fold_column("./lanzar5/150gen STRONGDENY/CSVs")
-listIndAlpha05AllowData <- add_fold_column("./lanzar6/150gen GRANTED/CSVs")
-listIndAlpha05DenyData <- add_fold_column("./lanzar6/150gen STRONGDENY/CSVs")
-listIndAlpha0AllowData <- add_fold_column("./lanzar7/150gen GRANTED/CSVs")
-listIndAlpha0DenyData <- add_fold_column("./lanzar7/150gen STRONGDENY/CSVs")
-listIndAlpha1AllowData <- add_fold_column("./lanzar8/150gen GRANTED/CSVs")
-listIndAlpha1DenyData <- add_fold_column("./lanzar8/150gen STRONGDENY/CSVs")
+add_fold_column("./lanzar5/150gen GRANTED/CSVs")
+add_fold_column("./lanzar5/150gen STRONGDENY/CSVs")
+add_fold_column("./lanzar6/150gen GRANTED/CSVs")
+add_fold_column("./lanzar6/150gen STRONGDENY/CSVs")
+add_fold_column("./lanzar7/150gen GRANTED/CSVs")
+add_fold_column("./lanzar7/150gen STRONGDENY/CSVs")
+add_fold_column("./lanzar8/150gen GRANTED/CSVs")
+add_fold_column("./lanzar8/150gen STRONGDENY/CSVs")
--- GPrules/dataprocessing.R
+++ GPrules/dataprocessing.R
@@ -9,15 +9,6 @@ library(grid)
 library(lattice)
 
 read_data <- function(directory) {
-  
-  i <- 0
-  for(inFile in list.files(path = directory, full.names = TRUE)) {
-    dataInFile <- read.table(file = inFile, header = TRUE, sep = ";")
-    dataInFile$FOLD <- paste("fold", i, sep = "")
-    write.table(dataInFile, file = inFile)
-    i <- i+1
-  }
-  
   allData <- do.call(rbind, lapply(list.files(path = directory, full.names = TRUE), read.table, sep = " "))
   return(allData)
 }
@@ -29,71 +20,78 @@ get_legend<-function(myggplot){
   return(legend)
 }
 
-alpha0Data <- read_data("./CSVs/a = 0")
-alpha05Data <- read_data("./CSVs/a = 0.5")
-alpha1Data <- read_data("./CSVs/a = 1")
+treeIndtreeIndAlpha05Data <- read_data("./lanzar1/CSVs")
+treeIndtreeIndAlpha0Data <- read_data("./lanzar2/CSVs")
+treeIndtreeIndAlpha1Data <- read_data("./lanzar3/CSVs")
+treeIndCovData <- read_data("./lanzar4/CSVs")
 
-allowData <- read_data("./CSVs/allow")
-denyData <- read_data("./CSVs/deny")
+listIndCovAllowData <- read_data("./lanzar5/150gen GRANTED/CSVs")
+listIndCovDenyData <- read_data("./lanzar5/150gen STRONGDENY/CSVs")
+listIndAlpha05AllowData <- read_data("./lanzar6/150gen GRANTED/CSVs")
+listIndAlpha05DenyData <- read_data("./lanzar6/150gen STRONGDENY/CSVs")
+listIndAlpha0AllowData <- read_data("./lanzar7/150gen GRANTED/CSVs")
+listIndAlpha0DenyData <- read_data("./lanzar7/150gen STRONGDENY/CSVs")
+listIndAlpha1AllowData <- read_data("./lanzar8/150gen GRANTED/CSVs")
+listIndAlpha1DenyData <- read_data("./lanzar8/150gen STRONGDENY/CSVs")
 
-Fa0 <- ggplot(alpha0Data, aes(x = alpha0Data$IT, y =BEST_F/47966)) +
+Fa0 <- ggplot(treeIndAlpha0Data, aes(x = treeIndAlpha0Data$IT, y =BEST_F/47966)) +
   geom_point(aes(colour = factor(FOLD))) + stat_summary() + scale_y_continuous(limits = c(0,0.5)) + theme(legend.position = "bottom")
 #theLegend <- get_legend(Fa0)
 Fa0 <- Fa0 + theme(legend.position = "none")
-Fa05 <- ggplot(alpha05Data, aes(x = alpha05Data$IT, y = alpha05Data$BEST_F/47966)) +
+Fa05 <- ggplot(treeIndAlpha05Data, aes(x = treeIndAlpha05Data$IT, y = treeIndAlpha05Data$BEST_F/47966)) +
   geom_point(aes(colour = factor(FOLD))) + stat_summary() + scale_y_continuous(limits = c(0,0.5))  + theme(legend.position = "none")
-Fa1 <- ggplot(alpha1Data, aes(x = alpha1Data$IT, y = alpha1Data$BEST_F/47966)) +
+Fa1 <- ggplot(treeIndAlpha1Data, aes(x = treeIndAlpha1Data$IT, y = treeIndAlpha1Data$BEST_F/47966)) +
   geom_point(aes(colour = factor(FOLD))) + stat_summary() + scale_y_continuous(limits = c(0,0.5))  + theme(legend.position = "none")
 
 #grid.arrange(Fa0, Fa05, Fa1, theLegend, ncol = 3, nrow = 2, layout_matrix = cbind(c(1, 4), c(2, 4), c(3, 4)))
 grid.arrange(Fa0, Fa05, Fa1, ncol = 3)
 
-Fallow <- ggplot(allowData, aes(x = allowData$IT, y = allowData$BEST_F/47966)) +
+Fallow <- ggplot(listIndCovAllowData, aes(x = listIndCovAllowData$IT, y = listIndCovAllowData$BEST_F/47966)) +
   geom_point(aes(colour = factor(FOLD))) + stat_summary()  + theme(legend.position = "bottom")
-Fdeny <- ggplot(denyData, aes(x = denyData$IT, y = denyData$BEST_F/47966)) +
+Fdeny <- ggplot(listIndCovDenyData, aes(x = listIndCovDenyData$IT, y = listIndCovDenyData$BEST_F/47966)) +
   geom_point(aes(colour = factor(FOLD))) + stat_summary() + theme(legend.position = "bottom")
 
 grid.arrange(Fallow, Fdeny, ncol = 2)
 
-Fallow0 <- ggplot(allowData[allowData$FOLD == "fold0",], aes(x = IT, y = BEST_F/47966)) +
+Fallow0 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold0",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fdeny0 <- ggplot(denyData[denyData$FOLD == "fold0",], aes(x = IT, y = BEST_F/47966)) +
+Fdeny0 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold0",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fallow1 <- ggplot(allowData[allowData$FOLD == "fold1",], aes(x = IT, y = BEST_F/47966)) +
+Fallow1 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold1",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fdeny1 <- ggplot(denyData[denyData$FOLD == "fold1",], aes(x = IT, y = BEST_F/47966)) +
+Fdeny1 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold1",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fallow2 <- ggplot(allowData[allowData$FOLD == "fold2",], aes(x = IT, y = BEST_F/47966)) +
+Fallow2 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold2",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fdeny2 <- ggplot(denyData[denyData$FOLD == "fold2",], aes(x = IT, y = BEST_F/47966)) +
+Fdeny2 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold2",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fallow3 <- ggplot(allowData[allowData$FOLD == "fold3",], aes(x = IT, y = BEST_F/47966)) +
+Fallow3 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold3",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fdeny3 <- ggplot(denyData[denyData$FOLD == "fold3",], aes(x = IT, y = BEST_F/47966)) +
+Fdeny3 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold3",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fallow4 <- ggplot(allowData[allowData$FOLD == "fold4",], aes(x = IT, y = BEST_F/47966)) +
+Fallow4 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold4",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fdeny4 <- ggplot(denyData[denyData$FOLD == "fold4",], aes(x = IT, y = BEST_F/47966)) +
+Fdeny4 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold4",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fallow5 <- ggplot(allowData[allowData$FOLD == "fold5",], aes(x = IT, y = BEST_F/47966)) +
+Fallow5 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold5",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fdeny5 <- ggplot(denyData[denyData$FOLD == "fold5",], aes(x = IT, y = BEST_F/47966)) +
+Fdeny5 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold5",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fallow6 <- ggplot(allowData[allowData$FOLD == "fold6",], aes(x = IT, y = BEST_F/47966)) +
+Fallow6 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold6",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fdeny6 <- ggplot(denyData[denyData$FOLD == "fold6",], aes(x = IT, y = BEST_F/47966)) +
+Fdeny6 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold6",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fallow7 <- ggplot(allowData[allowData$FOLD == "fold7",], aes(x = IT, y = BEST_F/47966)) +
+Fallow7 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold7",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fdeny7 <- ggplot(denyData[denyData$FOLD == "fold7",], aes(x = IT, y = BEST_F/47966)) +
+Fdeny7 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold7",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fallow8 <- ggplot(allowData[allowData$FOLD == "fold8",], aes(x = IT, y = BEST_F/47966)) +
+Fallow8 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold8",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fdeny8 <- ggplot(denyData[denyData$FOLD == "fold8",], aes(x = IT, y = BEST_F/47966)) +
+Fdeny8 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold8",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fallow9 <- ggplot(allowData[allowData$FOLD == "fold9",], aes(x = IT, y = BEST_F/47966)) +
+Fallow9 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold9",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
-Fdeny9 <- ggplot(denyData[denyData$FOLD == "fold9",], aes(x = IT, y = BEST_F/47966)) +
+Fdeny9 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold9",], aes(x = IT, y = BEST_F/47966)) +
   geom_point() + theme(legend.position = "bottom")
 
 grid.arrange(Fallow0, Fdeny0, Fallow1, Fdeny1, Fallow2, Fdeny2, Fallow3, Fdeny3, Fallow4, Fdeny4, Fallow5, Fdeny5, Fallow6, Fdeny6, Fallow7, Fdeny7, Fallow8, Fdeny8, Fallow9, Fdeny9, ncol = 2, nrow = 10)
