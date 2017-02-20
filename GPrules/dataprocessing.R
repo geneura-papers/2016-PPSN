aa115200222beb05f9ddd0de45507f28da4520b3
unintendedbear <unintendedbear@gmail.com>
2017-02-20 15:47:16 +0100

    Added graphs with results for individuals = 1 rule

--- GPrules/dataprocessing.R
+++ GPrules/dataprocessing.R
@@ -25,8 +25,8 @@ treeIndAlpha0Data <- read_data("./lanzar2/CSVs")
 treeIndAlpha1Data <- read_data("./lanzar3/CSVs")
 treeIndCovData <- read_data("./lanzar4/CSVs")
 
-listIndCovAllowData <- read_data("./lanzar5/150gen GRANTED/CSVs")
-listIndCovDenyData <- read_data("./lanzar5/150gen STRONGDENY/CSVs")
+listIndCovAllowData <- read_data("./lanzar5/100gen GRANTED/CSVs")
+listIndCovDenyData <- read_data("./lanzar5/100gen STRONGDENY/CSVs")
 listIndAlpha05AllowData <- read_data("./lanzar6/150gen GRANTED/CSVs")
 listIndAlpha05DenyData <- read_data("./lanzar6/150gen STRONGDENY/CSVs")
 listIndAlpha0AllowData <- read_data("./lanzar7/150gen GRANTED/CSVs")
@@ -62,52 +62,45 @@ FtreeCov <- ggplot(treeIndCovData, aes(x = treeIndCovData$IT, y =BEST_F)) +
   ylab("Best Fitness (Accuracy)") + theme(legend.position = "bottom")
 print(FtreeCov)
 
-Fallow <- ggplot(listIndCovAllowData, aes(x = listIndCovAllowData$IT, y = listIndCovAllowData$BEST_F/47966)) +
-  geom_point(aes(colour = factor(FOLD))) + stat_summary()  + theme(legend.position = "bottom")
-Fdeny <- ggplot(listIndCovDenyData, aes(x = listIndCovDenyData$IT, y = listIndCovDenyData$BEST_F/47966)) +
-  geom_point(aes(colour = factor(FOLD))) + stat_summary() + theme(legend.position = "bottom")
+FlistallowCov <- ggplot(listIndCovAllowData, aes(x = listIndCovAllowData$IT, y = listIndCovAllowData$BEST_F)) +
+  geom_line(aes(colour = factor(FOLD))) + stat_summary(geom = "line") + 
+  xlab("Iterations (class = GRANTED)") +
+  ylab("Best Fitness (Accuracy)") + theme(legend.position = "bottom")
+theLegend <- get_legend(FlistallowCov)
+FlistallowCov <- FlistallowCov + theme(legend.position = "none")
+FlistdenyCov <- ggplot(listIndCovDenyData, aes(x = listIndCovDenyData$IT, y = listIndCovDenyData$BEST_F)) +
+  geom_line(aes(colour = factor(FOLD))) + stat_summary(geom = "line") + 
+  xlab("Iterations (class = STRONGDENY)") +
+  ylab("Best Fitness (Accuracy)") + theme(legend.position = "none")
 
-grid.arrange(Fallow, Fdeny, ncol = 2)
+grid.arrange(FlistallowCov, FlistdenyCov, theLegend, ncol = 2, nrow = 2, layout_matrix = cbind(c(1, 3), c(2, 3)))
 
-Fallow0 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold0",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fdeny0 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold0",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fallow1 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold1",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fdeny1 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold1",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fallow2 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold2",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fdeny2 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold2",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fallow3 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold3",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fdeny3 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold3",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fallow4 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold4",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fdeny4 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold4",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fallow5 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold5",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fdeny5 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold5",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fallow6 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold6",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fdeny6 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold6",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fallow7 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold7",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fdeny7 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold7",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fallow8 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold8",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fdeny8 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold8",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fallow9 <- ggplot(listIndCovAllowData[listIndCovAllowData$FOLD == "fold9",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
-Fdeny9 <- ggplot(listIndCovDenyData[listIndCovDenyData$FOLD == "fold9",], aes(x = IT, y = BEST_F/47966)) +
-  geom_point() + theme(legend.position = "bottom")
+FlistallowAlpha0 <- ggplot(listIndAlpha0AllowData, aes(x = listIndAlpha0AllowData$IT, y = listIndAlpha0AllowData$BEST_F/47966)) +
+  geom_line(aes(colour = factor(FOLD))) + stat_summary(geom = "line")+
+  xlab("Iterations (α = 0) (class = GRANTED)") +
+  ylab("Best Fitness")
+theLegend <- get_legend(FlistallowAlpha0)
+FlistallowAlpha0 <- FlistallowAlpha0 + theme(legend.position = "none")
+FlistdenyAlpha0 <- ggplot(listIndAlpha0DenyData, aes(x = listIndAlpha0DenyData$IT, y = listIndAlpha0DenyData$BEST_F/47966)) +
+  geom_line(aes(colour = factor(FOLD))) + stat_summary(geom = "line")+
+  xlab("Iterations (α = 0) (class = STRONGDENY)") +
+  ylab("Best Fitness") + theme(legend.position = "none")
+FlistallowAlpha05 <- ggplot(listIndAlpha05AllowData, aes(x = listIndAlpha05AllowData$IT, y = listIndAlpha05AllowData$BEST_F/47966)) +
+  geom_line(aes(colour = factor(FOLD))) + stat_summary(geom = "line")+
+  xlab("Iterations (α = 0.5) (class = GRANTED)") +
+  ylab("Best Fitness") + theme(legend.position = "none")
+FlistdenyAlpha05 <- ggplot(listIndAlpha05DenyData, aes(x = listIndAlpha05DenyData$IT, y = listIndAlpha05DenyData$BEST_F/47966)) +
+  geom_line(aes(colour = factor(FOLD))) + stat_summary(geom = "line")+
+  xlab("Iterations (α = 0.5) (class = STRONGDENY)") +
+  ylab("Best Fitness") + theme(legend.position = "none")
+FlistallowAlpha1 <- ggplot(listIndAlpha1AllowData, aes(x = listIndAlpha1AllowData$IT, y = listIndAlpha1AllowData$BEST_F/47966)) +
+  geom_line(aes(colour = factor(FOLD))) + stat_summary(geom = "line")+
+  xlab("Iterations (α = 1) (class = GRANTED)") +
+  ylab("Best Fitness") + theme(legend.position = "none")
+FlistdenyAlpha1 <- ggplot(listIndAlpha1DenyData, aes(x = listIndAlpha1DenyData$IT, y = listIndAlpha1DenyData$BEST_F/47966)) +
+  geom_line(aes(colour = factor(FOLD))) + stat_summary(geom = "line")+
+  xlab("Iterations (α = 1) (class = STRONGDENY)") +
+  ylab("Best Fitness") + theme(legend.position = "none")
 
-grid.arrange(Fallow0, Fdeny0, Fallow1, Fdeny1, Fallow2, Fdeny2, Fallow3, Fdeny3, Fallow4, Fdeny4, Fallow5, Fdeny5, Fallow6, Fdeny6, Fallow7, Fdeny7, Fallow8, Fdeny8, Fallow9, Fdeny9, ncol = 2, nrow = 10)
+grid.arrange(FlistallowAlpha0, FlistdenyAlpha0, FlistallowAlpha05, FlistdenyAlpha05, FlistallowAlpha1, FlistdenyAlpha1, ncol = 2, nrow = 3)
+grid.arrange(FlistallowAlpha0, FlistdenyAlpha0, FlistallowAlpha05, FlistdenyAlpha05, FlistallowAlpha1, FlistdenyAlpha1, theLegend, ncol = 3, nrow = 3, layout_matrix = rbind(c(1, 2, 7), c(3, 4, 7), c(5, 6, 7)))
